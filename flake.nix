2cffaeb3f012b1a01b0b21971dce44904567a3ab
IanKitchenh <76395157+IanKitchenh@users.noreply.github.com>
2025-01-05 18:30:38 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.shittop = nixpkgs.lib.nixosSystem{
+    system = "x86_64-linux";
+    modules = [./configuration.nix];
+    };
+  };
+}
